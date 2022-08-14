e1e6af1fd867497131f728ab88f21738c2daa890
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-13 22:47:49 -0300

    novedades

--- js/products.js
+++ js/products.js
@@ -3,40 +3,34 @@ let categoriesArray = [];
 
 //función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
 function showCategoriesList(array) {
-    let htmlContentToAppend = "";
-
     for (let i = 0; i < array.length; i++) {
-        let category = array[i];
-        htmlContentToAppend += `
+        document.getElementById("cat-list-container").innerHTML += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                    <img src="` + array[i].image + `" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1">
-                        <h4>` + category.name + " - " + category.currency + " " + category.cost + `</h4> 
-                        <p> ` + category.description + `</p> 
+                        <h4>` + array[i].name + " - " + array[i].currency + " " + array[i].cost + `</h4> 
+                        <p> ` + array[i].description + `</p> 
                         </div>
-                        <small class="text-muted">` + category.soldCount + ` vendidos</small> 
+                        <small class="text-muted">` + array[i].soldCount + ` vendidos</small> 
                     </div>
 
                 </div>
             </div>
         </div>
-        `
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        `;
     }
 }
 
-//DOMContentLoaded
 document.addEventListener("DOMContentLoaded", function (e) {
     //Inicio DOMContentLoaded
     getJSONData(AUTOS).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray.products);
+            showCategoriesList(resultObj.data.products);
         }
     });
     //Fin de DOMContentLoaded
