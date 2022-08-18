86a9dc6fa2fea664de71df8586227565eadef623
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-17 22:11:15 -0300

    a

--- js/login.js
+++ js/login.js
@@ -5,38 +5,12 @@
            login();
        })
 
-       const {google} = require('518606529706-6v48q9k3l67p3mvgvnm1b5n8fj73r14k.apps.googleusercontent.com');
-
-/**
- * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
- * from the client_secret.json file. To get these credentials for your application, visit
- * https://console.cloud.google.com/apis/credentials.
- */
-const oauth2Client = new google.auth.OAuth2(
-    "518606529706-6v48q9k3l67p3mvgvnm1b5n8fj73r14k.apps.googleusercontent.com",
-  "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",
-  "index.html"
-);
-
-// Access scopes for read-only Drive activity.
-const scopes = [
-  'https://www.googleapis.com/auth/drive.metadata.readonly'
-];
-
-// Generate a url that asks permissions for the Drive activity scope
-const authorizationUrl = oauth2Client.generateAuthUrl({
-  // 'online' (default) or 'offline' (gets refresh_token)
-  access_type: 'offline',
-  /** Pass in the scopes array defined above.
-    * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-  scope: scopes,
-  // Enable incremental authorization. Recommended as a best practice.
-  include_granted_scopes: true
-});
        //Fin del DOMContenLoaded
    });
 //login google
-
+// Credenciales de cliente
+let client_id = '518606529706-ffieeft9dm0b57bflr1dbkp6ip2v9rn8.apps.googleusercontent.com';
+let client_secret = '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';
 
 
    //Funcion para intentar logear
