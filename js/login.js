5a16cca533460aa583a1c4a98d61edaeb84d96a2
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-20 00:28:26 -0300

    asd

--- js/login.js
+++ js/login.js
@@ -49,26 +49,4 @@ let client_secret = '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';
    //sessionStorage.clear;
    //localStorage.removeItem("user");
 
-   $(document).ready(function(){
-	$("#signout-container").hide();
-});
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  $("#signout-container").show();
-  $("#signin-container").hide();
-  $("#loggedUserImage").attr("src", profile.getImageUrl());
-  $("#loggedUsername").html(profile.getName());
-  $("#loggedUserEmail").html(profile.getEmail());
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut().then(function () {
-	  console.log('User signed out.');
-	  $("#signout-container").hide();
-	  $("#signin-container").show();
-	});
-}
+   
