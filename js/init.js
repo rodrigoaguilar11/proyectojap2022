53f10d932b6a29e6df3cb1fa391ae6a7ef07bec5
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-22 01:07:06 -0300

    test

--- js/init.js
+++ js/init.js
@@ -10,47 +10,45 @@ const EXT_TYPE = ".json";
 const AUTOS = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
 
-let showSpinner = function(){
+let showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-let hideSpinner = function(){
+let hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
+let getJSONData = function (url) {
+  let result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   //Detectar Sesion
-  if (localStorage.getItem("username")==undefined || localStorage.getItem("password")==undefined) {
-      location.href = "login.html";
-  } else {
-      document.getElementById("profile").innerHTML +='<span style= color:lightblue; font-size:30px; font-weight:bold;>'+localStorage.getItem("username") +'</span>' ;
+  if (localStorage.getItem("username") == undefined || localStorage.getItem("password") == undefined) {
+    location.href = "login.html";
   }
+  document.getElementById("profile").innerHTML += '<span style= color:lightblue; font-size:30px; font-weight:bold;>' + localStorage.getItem("username") + '</span>';
+
   //Fin de DOMContentLoaded
 });
-
-
