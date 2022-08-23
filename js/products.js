793af2b81733d40bd135869a9e9317843492c613
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-23 19:22:13 -0300

    Filter

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 let categoriesArray = [];
 
 function showCategoriesList(array) {
-    document.getElementById("cat-list-container").innerHTML="";
+    document.getElementById("cat-list-container").innerHTML = "";
     for (let i = 0; i < array.length; i++) {
         document.getElementById("cat-list-container").innerHTML += `
         <div class="list-group-item list-group-item-action">
@@ -28,34 +28,34 @@ function showCategoriesList(array) {
 document.addEventListener("DOMContentLoaded", function (e) {
     //Inicio DOMContentLoaded
     let catID;
-    let productsh1 =document.getElementById("productsh1");
+    let productsh1 = document.getElementById("productsh1");
     if (localStorage.getItem("catID") == "101") {
         catID = AUTOS;
-        productsh1.innerHTML="Autos";
+        productsh1.innerHTML = "Autos";
     } else if (localStorage.getItem("catID") == "102") {
         catID = JUGUETES;
-        productsh1.innerHTML="Juguetes";
+        productsh1.innerHTML = "Juguetes";
     } else if (localStorage.getItem("catID") == "103") {
         catID = MUEBLES;
-        productsh1.innerHTML="Muebles";
+        productsh1.innerHTML = "Muebles";
     } else if (localStorage.getItem("catID") == "104") {
         catID = HERRAMIENTAS;
-        productsh1.innerHTML="Herramientas";
+        productsh1.innerHTML = "Herramientas";
     } else if (localStorage.getItem("catID") == "105") {
         catID = COMPUTADORAS;
-        productsh1.innerHTML="Computadoras";
+        productsh1.innerHTML = "Computadoras";
     } else if (localStorage.getItem("catID") == "106") {
         catID = VESTIMENTA;
-        productsh1.innerHTML="Vestimenta";
+        productsh1.innerHTML = "Vestimenta";
     } else if (localStorage.getItem("catID") == "107") {
         catID = ELECTRODOMESTICOS;
-        productsh1.innerHTML="Electrodomesticos";
+        productsh1.innerHTML = "Electrodomesticos";
     } else if (localStorage.getItem("catID") == "108") {
         catID = DEPORTE;
-        productsh1.innerHTML="Deporte";
+        productsh1.innerHTML = "Deporte";
     } else if (localStorage.getItem("catID") == "109") {
         catID = CELULARES;
-        productsh1.innerHTML="Celulares";   
+        productsh1.innerHTML = "Celulares";
     }
     //Asignar ID a la Lista
     let productos;
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         if (resultObj.status === "ok") {
             showCategoriesList(resultObj.data.products);
             productos = (resultObj.data.products);
-            console.log(productos);
+            //console.log(productos);
         }
     });
     //Ordenadores
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 return 1;
             } else {
                 return 0;
-            }    
+            }
         });
         showCategoriesList(productos);
     })
@@ -103,10 +103,39 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 return 1;
             } else {
                 return 0;
-            }    
+            }
         });
         showCategoriesList(productos);
 
     })
+    //Filtro por precio
+    document.getElementById("rangeFilterCount").addEventListener("click", () => {
+        let min = document.getElementById("filterMin").value;
+        let max = document.getElementById("filterMax").value;
+
+        if (min == "") {
+            min = "0";
+        } else if (max == "") {
+            max = "99999999999";
+        }
+        let result = productos.filter(p => p.cost >= min && p.cost <= max);
+        result.sort((a, b) => a.cost - b.cost);
+        showCategoriesList(result);
+
+    })
+    document.getElementById("clearRangeFilter").addEventListener("click", () => {
+        document.getElementById("filterMax").value = "";
+        document.getElementById("filterMin").value = "";
+    })
+
     //Fin de DOMContentLoaded
 });
+
+//Ordenar lista
+function filtrar() {
+    let inicial = parseInt(document.getElementById("inicio").value);
+    let final = parseInt(document.getElementById("final").value);
+    let listaFiltrada = personas.filter(persona => persona.edad >= inicial && persona.edad <= final);
+    listaFiltrada.sort((ant, sig) => ant.edad - sig.edad);
+    mostrar(listaFiltrada);
+}
